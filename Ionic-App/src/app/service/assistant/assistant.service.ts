db552124fcaa0c75b728fd0cf0a67d9dc95df9b2
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2019-10-07 11:41:08 +0200

    Publish to azure and bind assistant service to api

--- Ionic-App/src/app/service/assistant/assistant.service.ts
+++ Ionic-App/src/app/service/assistant/assistant.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AssistantService {
-  constructor(private alertController: AlertController) {}
+  private apiBaseUrl = 'http://siglatechdialogflow.azurewebsites.net/dialogflow/it_IT/';
+
+  constructor(
+    private alertController: AlertController,
+    private httpClient: HttpClient
+    ) {}
 
   getReply(query: string): Observable<string> {
-    return of(`You said: ${query}`);
+    const post = this.httpClient.post(this.apiBaseUrl, query);
+    return post.pipe(map(obj => (obj as any).responseText.text[0]));
   }
 }
