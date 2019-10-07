e66c58a33af16bd9155de4ff33dfa6e94e80c690
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2019-10-07 10:48:47 +0200

    Add backend

---
+++ Backend/API/API/Controllers/DialogflowController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Google.Cloud.Dialogflow.V2;
+using Google.Apis.Auth.OAuth2;
+using Grpc.Auth;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DialogflowController : ControllerBase
+    {
+        private readonly ILogger<DialogflowController> _logger;
+
+        public DialogflowController(ILogger<DialogflowController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("query/{sessionId}/{langCode}")]
+        public ActionResult<Models.DataflowResponse> Query(string sessionId, string langCode, [FromBody] string rawQuery)
+        {
+            const string agent = "notespese-nsxqke";
+            const string credFile = "Credentials/google.json";
+
+            if (!System.IO.File.Exists(credFile))
+            {
+                return BadRequest("No Google credentials provided.");
+            }
+
+            var creds = GoogleCredential.FromJson(credFile);
+            var channel = new Grpc.Core.Channel(SessionsClient.DefaultEndpoint.Host,
+                          creds.ToChannelCredentials());
+            var client = SessionsClient.Create(channel);
+
+            var query = new QueryInput()
+            {
+                Text = new TextInput()
+                {
+                    Text = rawQuery,
+                    LanguageCode = langCode
+                }
+            };
+
+            var dialogFlow = client.DetectIntent(
+                new SessionName(agent, sessionId),
+                query
+            );
+
+            channel.ShutdownAsync();
+
+            return new ActionResult<Models.DataflowResponse>(new Models.DataflowResponse()
+            {
+                ResponseText = dialogFlow.QueryResult.FulfillmentMessages.First().Text.ToString()
+            });
+        }
+    }
+}
